d0f4d196f8b62e9fb741a0caec2759efb0ddb8ee
Jerry Chen <jzc23@pitt.edu>
2022-05-11 21:06:49 -0400

    going thru it

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { 
+  Button,
+  StyleSheet,
+  SafeAreaView,
+  Alert
+} from 'react-native';
 
 export default function App() {
+   
   return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={[styles.container, containerStyle]}>
+      <Button
+        title="Click Me" 
+        onPress={() => console.log("Button Pressed")}
+      />
+    </SafeAreaView>
   );
 }
+
+const containerStyle = { backgroundColor: "orange" }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
 });
